0afd3418bc4d32657453bbab923a296335d5db85
Emily Spoldi <emily.l.spoldi@gmail.com>
2020-12-09 20:24:19 -0500

    history now prints on the side

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 // City Data Variables
 var cityName = "";
-var lat = 0;
-var lon = 0;
 
 // API key
 var APIKey = "b3ba97da6b3e519c3ebd004958f21e41";
 
 // URLs to pull from API
-var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+var currentWeather = `https://api.openweathermap.org/data/2.5/weather?q=&appid=${APIKey}`;
 
-var forcastFive = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIKey}`;
+var forcastFive = `https://api.openweathermap.org/data/2.5/forecast?q=&appid=${APIKey}`;
 
-var uvIndex = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+var uvIndex = `https://api.openweathermap.org/data/2.5/uvi?lat=&lon=&appid=${APIKey}`;
 
 // Listen for city name submission
 $("form").submit(function(event) {
     event.preventDefault();
     cityName = $("#city")[0].value;
 
-    currentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+    currentWeather = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}`;
 
     forcastFive = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIKey}`;
-
-    uvIndex = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${APIKey}`;
 
     getDetails();
 });
@@ -32,8 +28,12 @@ function getDetails() {
         url: currentWeather,
         method: "GET"
       }).then(function(response) {
+        uvIndex = `https://api.openweathermap.org/data/2.5/uvi?lat=${response.coord.lat}&lon=${response.coord.lon}&appid=${APIKey}`;
 
           console.log(currentWeather);
           console.log(response);
+          console.log(uvIndex);
+          
+          $("section").prepend(`<hr>${response.name}`);
         });
 };
