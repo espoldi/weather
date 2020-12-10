6343d9007aa1184793abd0369b8785f6ab9f33dd
Emily Spoldi <emily.l.spoldi@gmail.com>
2020-12-09 22:34:13 -0500

    added forcast card data

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var forcastFive = `https://api.openweathermap.org/data/2.5/forecast?q=&appid=${A
 var uvIndex = `https://api.openweathermap.org/data/2.5/uvi?lat=&lon=&appid=${APIKey}`;
 
 // Listen for city name submission
-$("form").submit(function(event) {
+$("form").submit(function (event) {
     event.preventDefault();
     cityName = $("#city")[0].value;
 
@@ -28,35 +28,58 @@ function getDetails() {
     $.ajax({
         url: currentWeather,
         method: "GET"
-      })
-      
-      .then(function(response) {
-        console.log(response);
-        //API call for UV Index information
-        uvIndex = `https://api.openweathermap.org/data/2.5/uvi?lat=${response.coord.lat}&lon=${response.coord.lon}&appid=${APIKey}`;
-        
-        //Add to search history
-        $("section").prepend(`<hr><button id=${response.name}>${response.name}`);
+    })
 
-        // Convert the temp to fahrenheit
-        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        .then(function (response) {
+            //API call for UV Index information
+            uvIndex = `https://api.openweathermap.org/data/2.5/uvi?lat=${response.coord.lat}&lon=${response.coord.lon}&appid=${APIKey}`;
 
-        //Display weather panel
-        $(".name").text(`${response.name}`);
-        $(".temp").text(`Temperature: ${tempF.toFixed(2)}`);
-        $(".humidity").text(`Humidity: ${response.main.humidity}%`);
-        $(".wind").text(`Wind Speed: ${response.wind.speed}MPH`);
+            //Add to search history
+            $("section").prepend(`<hr><button id=${response.name}>${response.name}`);
 
-        //Call for UV Index
-        $.ajax({
-            url: uvIndex,
-            method: "GET"
+            // Convert the temp to fahrenheit
+            var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+            //Display weather panel
+            $(".name").text(`${response.name}`);
+            $(".temp").text(`Temperature: ${tempF.toFixed(2)}`);
+            $(".humidity").text(`Humidity: ${response.main.humidity}%`);
+            $(".wind").text(`Wind Speed: ${response.wind.speed}MPH`);
+
+
+            //Call for UV Index
+            $.ajax({
+                url: uvIndex,
+                method: "GET"
+            })
+                .then(function (response) {
+                    //Display UV
+                    $(".UV").text(`UV Index: ${response.value}`);
+                })
+
+            //Call for forcast
+            $.ajax({
+                url: forcastFive,
+                method: "GET"
+            })
+                .then(function (response) {
+                    console.log(response);
+                    //Loop for each day
+                    for (let i = 0; i < 5; i++) {
+                        
+                        let spot = 8*i + 3;
+                        
+                        let tempF = (response.list[spot].main.temp - 273.15) * 1.80 + 32;
+                        
+                        //Block Print out
+                        $(`#day${i}`).html(`${response.list[spot].dt_txt}<br>
+                        ${response.list[spot].weather[0].main}<br>
+                        <img src='http://openweathermap.org/img/wn/${response.list[spot].weather[0].icon}@2x.png'><br>
+                        Temperature: ${tempF.toFixed(2)}F<br>
+                        Humidity: ${response.list[spot].main.humidity}%`);
+                    };
+                })
+
+
         })
-        .then(function(response) {
-            console.log(response);
-            //Display UV
-            $(".UV").text(`UV Index: ${response.value}`);
-        })
-        
-      })
 };
